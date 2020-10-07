c7b7f67c686de894beb6b2ae590a082b2a194af3
deep846 <45318452+deep846@users.noreply.github.com>
2020-10-07 18:33:39 +0530

    deeep

---
+++ DEEP CHOUDHURY.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include io.h>
+
+int main()
+{
+    int 0,r=10,t=1;
+    printf("p=%d\n",p);
+    printf("
+    
+    printf("Interest= (%d*%d*%d)/100=%d",p,r,t,(p*r*t)/100);
+    
+    
+    return 0;
+}
